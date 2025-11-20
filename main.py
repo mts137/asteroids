2a90358cd00ecaca0edd347ec0a97a7406e3eea0
mts137 <icdeo@proton.me>
2025-11-20 21:17:13 +0100

    add delta time

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -16,6 +18,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
